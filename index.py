ae9a060aa8809d8fb3418b6b42ea543b45f23505
Anton718 <anton.kostroma@yahoo.com>
2024-06-09 07:42:17 -0400

    add

--- index.py
+++ index.py
@@ -1,6 +1,35 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as mpl
+import os
 
-table1 = pd.read_csv('./data/April2023/dot1_0423.csv')
-print(table1.info())
+data = './data'
+
+dataContents = os.listdir(data)
+
+months2023 = []
+
+for dir in dataContents:
+    months2023.append(dir)
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+
+sortedMonths2023 = []
+
+for month in months:
+    for month2023 in months2023:
+        if month in month2023 or month.lower() in month2023:
+            sortedMonths2023.append(month2023)
+
+orderedLinks = []
+
+for month in sortedMonths2023:
+    folderContents = os.listdir(data + F"/{month}")
+    folderContents.sort()
+    for file in folderContents:
+        orderedLinks.append(data + f"/{month}/{file}")
+
+for link in orderedLinks:
+    df = pd.read_csv(link, dtype={"MEXSTATE": "string", "CONTCODE": "string"})
+    print(df)
+
