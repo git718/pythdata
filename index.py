177f0cbaa166807fb1ca208ae8034e84db5495aa
git718 <anton.kostroma@yahoo.com>
2024-08-20 07:31:39 -0400

    add

--- index.py
+++ index.py
@@ -36,10 +36,18 @@ for link in orderedLinks:
     allDataFrames.append(df)
 
 oneData = pd.concat(allDataFrames)
-print(oneData.info())
-x = np.array(oneData["MONTH"])
-y = np.array(oneData["FREIGHT_CHARGES"])
-mpl.xlabel("months")
-mpl.ylabel("freight charges")
-mpl.scatter(x, y)
-mpl.show()
+# print(oneData.info())
+x = np.unique(np.array(oneData["MONTH"]))
+y = []
+print(x)
+for i in x:
+    values = np.array(oneData["VALUE"])
+    if len(str(i)) < 2:
+        i = "0" + str(i)
+    monthly_total_amounts = np.sum(values, where="MONTH" == i)
+    y.append(monthly_total_amounts)
+print(y)
+# mpl.xlabel("months")
+# mpl.ylabel("value")
+# mpl.scatter(x, y)
+# mpl.show()
