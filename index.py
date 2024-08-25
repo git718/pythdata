8e9e9c3d67e418f38efe52af2c8e0715ebad37c3
git718 <anton.kostroma@yahoo.com>
2024-08-25 14:16:11 -0400

    add

--- index.py
+++ index.py
@@ -36,24 +36,41 @@ for link in orderedLinks:
     allDataFrames.append(df)
 
 oneData = pd.concat(allDataFrames)
-y = []
-x = np.unique(np.array(oneData["MONTH"]))
-for i in x:
-    newdata = oneData.loc[oneData["MONTH"] == i]
-    total = np.sum(np.array(newdata["VALUE"]))
-    y.append(total)
-mpl.xlabel("months")
-mpl.ylabel("value")
-mpl.plot(x, y)
-mpl.show()
+# y = []
+# x = np.unique(np.array(oneData["MONTH"]))
+# for i in x:
+#     newdata = oneData.loc[oneData["MONTH"] == i]
+#     total = np.sum(np.array(newdata["VALUE"]))
+#     y.append(total)
+# mpl.xlabel("months")
+# mpl.ylabel("value")
+# mpl.plot(x, y)
+# mpl.show()
 
-y1 = []
-x1 = np.unique(np.array(oneData["MONTH"]))
-for i in x1:
-    newdata1 = oneData.loc[oneData["MONTH"] == i]
-    total1 = np.sum(np.array(newdata1["SHIPWT"]))
-    y1.append(total1)
-mpl.xlabel("months")
-mpl.ylabel("weight")
-mpl.plot(x1, y1)
-mpl.show()
+# y1 = []
+# x1 = np.unique(np.array(oneData["MONTH"]))
+# for i in x1:
+#     newdata1 = oneData.loc[oneData["MONTH"] == i]
+#     total1 = np.sum(np.array(newdata1["SHIPWT"]))
+#     y1.append(total1)
+# mpl.xlabel("months")
+# mpl.ylabel("weight")
+# mpl.plot(x1, y1)
+# mpl.show()
+
+eight_month = oneData.loc[oneData["MONTH"] == 8]
+print(eight_month.mode())
+
+commodities_arr = []
+for goods_code in range(0, 99):
+    if len(str(goods_code)) < 2:
+        goods_code = "0" + str(goods_code)
+    else:
+        goods_code = str(goods_code)
+    commodities_arr.append(goods_code)
+
+print(eight_month)
+result = {}
+for i in commodities_arr:
+    sum = np.sum(np.array(eight_month["SHIPWT"] == i))
+    result[i] = sum
