ee0479d53c1b00a524a8b54c0152bed3058250bd
git718 <anton.kostroma@yahoo.com>
2024-08-28 07:16:40 -0400

    add

--- index.py
+++ index.py
@@ -25,3 +25,19 @@ while counter < len(i):
     for i in month_data:
         sorted_dates.append(i[counter])
     counter = counter + 1
+
+df_trucks = df.loc[df["Measure"] == "Trucks"]
+# we get information only about trucks crossing border in the period between 1996 and 2023
+trucks_data = []
+for i in sorted_dates:
+    df_monthly_trucks = df_trucks.loc[df_trucks["Date"] == i]
+    trucks_per_month = np.sum(np.array(df_monthly_trucks["Value"]))
+    trucks_data.append(trucks_per_month)
+
+x = sorted_dates
+y = trucks_data
+mpl.xlabel("1996 - 2023 yy")
+mpl.ylabel("Trucks crossing border")
+mpl.plot(x, y)
+mpl.show()
+# Figure A shows that sudden drops happened in year 2008 (during global economic shock) and in 2020 (approximately April) which could be due to Covid situation.
